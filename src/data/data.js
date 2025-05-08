d227ded23bb2acac0c4dfc1ce7bcca6166297bc7
Mahesh <mahesh001.mg@gmail.com>
2025-05-08 21:40:12 +0530

    Projects added

--- src/data/data.js
+++ src/data/data.js
@@ -1,29 +1,27 @@
-import WorkIm from '../assets/projects/workImg.jpeg'
-import RealEst from '../assets/projects/realestate.jpg'
-
-export const data=[
-    {
-        id:1,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
-    },
-    {
-        id:2,
-        name:"React JS Application",
-        image:RealEst,
-        github:"",
-        live:"",
-    },
-    {
-        id:3,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
-    },
-    
-
-
-]
+import WorkIm from "../assets/projects/workImg.jpeg";
+import RealEst from "../assets/projects/realestate.jpg";
+import Theif from "../assets/projects/thiefDetection.jpg";
+import Todo from "../assets/projects/todo.jpg";
+export const data = [
+  {
+    id: 1,
+    name: "Thief Detection Alarm Application",
+    image: Theif,
+    github: "https://github.com/maheshgit-hub/object-detection-nextjs-ai",
+    live: "https://aiobjectsdetection.netlify.app/",
+  },
+  {
+    id: 2,
+    name: "Todo Application",
+    image: Todo,
+    github: "https://github.com/maheshgit-hub/todoApp",
+    live: "https://todo-rect.netlify.app/",
+  },
+  {
+    id: 3,
+    name: "React JS Application",
+    image: WorkIm,
+    github: "",
+    live: "",
+  },
+];
